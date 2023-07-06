5eeb215a3705bad74b9c2c5e8fbf643e0f89a599
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-07-06 20:45:41 -0300

    bad smells removed

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    //READ cRud
     function loadTasks() {
         $.ajax({
             url: 'crud_php/load_tasks.php',
             type: 'GET',
             success: function (response) {
-                $('#tasks').empty();
+                $('#tasks').empty()
                 response.forEach(function (task) {
-                    //var listItem = $('<li>').text(task.descricao + ' ' + task.prazo + ' ' + task.prioridade)
                     
                     var listItem = $('<div>')
                     listItem.addClass('task')
@@ -16,46 +14,45 @@ $(document).ready(function () {
                     var descricaoDiv = $('<div>').text(task.descricao)
                     descricaoDiv.addClass('descricao')
                     contentDiv.append(descricaoDiv)
-                    // Cria uma div para o prazo da tarefa
+            
                     var prazoDiv = $('<div>').text('Prazo: '+task.prazo)
-                    prazoDiv.addClass('prazo') // Adiciona a classe 'prazo' à div
-                    contentDiv.append(prazoDiv) // Adiciona a div de prazo à div principal
+                    prazoDiv.addClass('prazo') 
+                    contentDiv.append(prazoDiv)
 
-                    // Cria uma div para a prioridade da tarefa
                     var prioridadeDiv = $('<div>').text('Prioridade: '+task.prioridade)
-                    prioridadeDiv.addClass('prioridade') // Adiciona a classe 'prioridade' à div
+                    prioridadeDiv.addClass('prioridade') 
                     contentDiv.append(prioridadeDiv)
                     contentDiv.addClass('content')
 
                     listItem.append(contentDiv)
-                    var deleteIcon = $('<img>').attr('src', 'imgs/lixeira.png').addClass('delete-icon');
-                    var editIcon = $('<img>').attr('src', 'imgs/edit.png').addClass('edit-icon');
-                    listItem.attr('data-task-id', task.id);
-                    listItem.attr('data-task-descricao', task.descricao);
-                    listItem.attr('data-task-prazo', task.prazo);
+                    var deleteIcon = $('<img>').attr('src', 'imgs/lixeira.png').addClass('delete-icon')
+                    var editIcon = $('<img>').attr('src', 'imgs/edit.png').addClass('edit-icon')
+                    listItem.attr('data-task-id', task.id)
+                    listItem.attr('data-task-descricao', task.descricao)
+                    listItem.attr('data-task-prazo', task.prazo)
                     listItem.attr('data-task-prioridade', task.prioridade)
-                    $('#tasks').append(listItem);
+                    $('#tasks').append(listItem)
                     listItem.append(editIcon)
-                    listItem.append(deleteIcon);
-                });
+                    listItem.append(deleteIcon)
+                })
             }
-        });
+        })
     }
-    loadTasks();
-    //CREATE Crud
+    loadTasks()
+    
     $('#task-form').submit(function (e) {
-        e.preventDefault();
-        var form = $(this);
+        e.preventDefault()
+        var form = $(this)
 
-        var descricao = $('#descricao').val();
-        var prazo = $('#prazo').val();
-        var prioridade = $('#prioridade').val();
+        var descricao = $('#descricao').val()
+        var prazo = $('#prazo').val()
+        var prioridade = $('#prioridade').val()
 
         var taskData = {
             descricao: descricao,
             prazo: prazo,
             prioridade: prioridade
-        };
+        }
 
         $.ajax({
             url: 'crud_php/create_task.php',
@@ -64,28 +61,27 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (response) {
                 if (response.success) {
-                    $('#task-id').val('');
-                    $('#descricao').val('');
-                    $('#prazo').val('');
-                    $('#prioridade').val('');
-                    $('#form').hide();
-                    $('#btn_send').hide();
-                    loadTasks();
+                    $('#task-id').val('')
+                    $('#descricao').val('')
+                    $('#prazo').val('')
+                    $('#prioridade').val('')
+                    $('#form').hide()
+                    $('#btn_send').hide()
+                    loadTasks()
                 } else {
-                    alert('Erro ao criar tarefa. Por favor, tente novamente.');
+                    alert('Erro ao criar tarefa. Por favor, tente novamente.')
                 }
             },
             error: function () {
-                alert('Erro ao comunicar com o servidor. Por favor, tente novamente.');
+                alert('Erro ao comunicar com o servidor. Por favor, tente novamente.')
             }
-        });
-    });
+        })
+    })
 
-    //DELETE cruD
+    
     $(document).on('click', '.delete-icon', function () {
-        var listItem = $(this).closest('.task');
-        var taskId = listItem.data('task-id');
-        // Confirmar exclusão da tarefa
+        var listItem = $(this).closest('.task')
+        var taskId = listItem.data('task-id')
         if (confirm('Tem certeza de que deseja excluir esta tarefa?')) {
             $.ajax({
                 url: 'crud_php/delete_task.php',
@@ -94,59 +90,56 @@ $(document).ready(function () {
                 dataType: 'json',
                 success: function (response) {
                     if (response.success) {
-                        listItem.remove(); // Remover o elemento <li> correspondente da lista
+                        listItem.remove()
                     } else {
-                        alert('Erro ao excluir tarefa. Por favor, tente novamente.');
+                        alert('Erro ao excluir tarefa. Por favor, tente novamente.')
                     }
                 },
                 error: function () {
-                    alert('Erro ao comunicar com o servidor. Por favor, tente novamente.');
+                    alert('Erro ao comunicar com o servidor. Por favor, tente novamente.')
                 }
-            });
+            })
         }
-    });
-    // UPDATE crUd
-    //Carregar os dados primeiro
+    })
+    
+    
     $(document).on('click', '.edit-icon', function (e) {
-        e.preventDefault();
-        var listItem = $(this).closest('.task');
-        //var descricao = listItem.find('.task-descricao').text();
-        var id = listItem.data('task-id');
-        var descricao = listItem.data('task-descricao');
-        var prazo = listItem.data('task-prazo');
-        var prioridade = listItem.data('task-prioridade');
+        e.preventDefault()
+        var listItem = $(this).closest('.task')
+        var id = listItem.data('task-id')
+        var descricao = listItem.data('task-descricao')
+        var prazo = listItem.data('task-prazo')
+        var prioridade = listItem.data('task-prioridade')
 
         $('#task-id').val(id)
-        $('#descricao').val(descricao);
-        $('#prazo').val(prazo);
-        $('#prioridade').val(prioridade);
-        $('#send').hide();
-        $('#form').show();
-        $('#update').show();
-        const title_form = $('#title_form');
-        title_form.text('Editar tarefa');
-    });
+        $('#descricao').val(descricao)
+        $('#prazo').val(prazo)
+        $('#prioridade').val(prioridade)
+        $('#send').hide()
+        $('#form').show()
+        $('#update').show()
+        const title_form = $('#title_form')
+        title_form.text('Editar tarefa')
+    })
 
 
     $(document).on('click', '#update', function (e) {
 
-        e.preventDefault();
-        // Obtém os dados atualizados do formulário
+        e.preventDefault()
         var id = $('#task-id').val()
-        var descricao = $('#descricao').val();
-        var prazo = $('#prazo').val();
-        var prioridade = $('#prioridade').val();
+        var descricao = $('#descricao').val()
+        var prazo = $('#prazo').val()
+        var prioridade = $('#prioridade').val()
 
-        // Cria um objeto com os dados atualizados da tarefa
+       
         var taskData = {
             taskId: id,
             descricao: descricao,
             prazo: prazo,
             prioridade: prioridade
-        };
+        }
         console.log('aqui')
         console.log(taskData)
-        // Envie a requisição Ajax para atualizar a tarefa
         $.ajax({
             url: 'crud_php/update_task.php',
             type: 'POST',
@@ -154,26 +147,22 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (response) {
                 if (response.success) {
-                    // Atualizar a lista de tarefas após a atualização bem-sucedida
-
-                    // Ocultar o formulário de edição
-
-                    // Limpar os campos do formulário
-                    $('#task-id').val('');
-                    $('#descricao').val('');
-                    $('#prazo').val('');
-                    $('#prioridade').val('');
-                    $('#form').hide();
-                    $('#update').hide();
-                    loadTasks();
+                    
+                    $('#task-id').val('')
+                    $('#descricao').val('')
+                    $('#prazo').val('')
+                    $('#prioridade').val('')
+                    $('#form').hide()
+                    $('#update').hide()
+                    loadTasks()
                 } else {
-                    alert('Erro ao atualizar as informações. Por favor, tente novamente.');
+                    alert('Erro ao atualizar as informações. Por favor, tente novamente.')
                 }
             },
             error: function () {
-                alert('Erro ao comunicar com o servidor. Por favor, tente novamente.');
+                alert('Erro ao comunicar com o servidor. Por favor, tente novamente.')
             }
-        });
-    });
-});
+        })
+    })
+})
 
